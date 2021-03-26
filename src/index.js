bfd59f842fc130becbe35645dca99d1de90c24c1
Mason Ellwood <masonellwood@gmail.com>
2021-03-25 18:43:20 -0700

    added props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-            <CommentDetail author="Alex" timeAgo="Yesterday at 4:45PM" />
-            <CommentDetail author="Jane" timeAgo="December at 4:45PM" />
+            <CommentDetail 
+                avatar={faker.image.image()} 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                comment="Nice blog post!" 
+            />
+            <CommentDetail 
+                avatar={faker.image.image()} 
+                author="Alex" 
+                timeAgo="Yesterday at 4:45PM" 
+                comment="Nice blog post!" 
+            />
+            <CommentDetail 
+                avatar={faker.image.image()} 
+                author="Jane" 
+                timeAgo="December at 4:45PM" 
+                comment="Nice blog post!" 
+            />
         </div>
     );
 };
