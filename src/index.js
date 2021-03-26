e1f7cb30776e8bb0372418f5aad1571465397939
Mason Ellwood <masonellwood@gmail.com>
2021-03-25 19:10:30 -0700

    approval card added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                avatar={faker.image.image()} 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                comment="Nice blog post!" 
-            />
-            <CommentDetail 
-                avatar={faker.image.image()} 
-                author="Alex" 
-                timeAgo="Yesterday at 4:45PM" 
-                comment="Nice blog post!" 
-            />
-            <CommentDetail 
-                avatar={faker.image.image()} 
-                author="Jane" 
-                timeAgo="December at 4:45PM" 
-                comment="Nice blog post!" 
-            />
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sureyou want to do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.image()} 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    comment="Nice blog post!" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.image()} 
+                    author="Alex" 
+                    timeAgo="Yesterday at 4:45PM" 
+                    comment="Nice blog post!" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.image()} 
+                    author="Jane" 
+                    timeAgo="December at 4:45PM" 
+                    comment="Nice blog post!" 
+                />
+            </ApprovalCard>
         </div>
     );
 };
